7d2651c4e005724a75bc161f9fe60b6d5f768a8e
Joao Galeao <jbgaleao@gmail.com>
2022-06-25 00:07:20 -0300

    Add project files.

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace many_to_many
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="many_to_many.MvcApplication" Language="C#" %>
